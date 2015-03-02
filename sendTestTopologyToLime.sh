12f481ee60809249bd3284613dbd197d9bec1195
Michael Coughlin <michael.coughlin@colorado.edu>
2015-03-02 00:07:55 -0700

    updated sending topology to match changes in api

--- sendTestTopologyToLime.sh
+++ sendTestTopologyToLime.sh
@@ -3,9 +3,9 @@
 original_ovx_switch="00:a4:23:05:00:00:00:01"
 clone_ovx_switch="00:a4:23:05:00:00:00:02"
 
-original_format_string="{\n\"%s\":\n{\"ports\":\n{\"1\":\"GHOST\",\n\"2\":\"H_CONNECTED\",\n\"3\":\"H_CONNECTED\"\n,\n\"4\":\"H_CONNECTED\",\n\"5\":\"H_CONNECTED\",\n\"6\":\"H_CONNECTED\"\n}\n}\n}"
+original_format_string="{\n\"%s\":\n{\"ports\":\n{\"1\":{\"type\":\"GHOST\"},\n\"2\":{\"type\":\"H_CONNECTED\",\"mac\":\"52:54:00:aa:52:b8\"},\n\"3\":{\"type\":\"H_CONNECTED\",\"mac\":\"52:54:00:49:a5:72\"}\n,\n\"4\":{\"type\":\"H_CONNECTED\",\"mac\":\"52:54:00:f5:e0:11\"},\n\"5\":{\"type\":\"H_CONNECTED\",\"mac\":\"52:54:00:43:cb:e3\"},\n\"6\":{\"type\":\"H_CONNECTED\",\"mac\":\"52:54:00:59:80:71\"}\n}\n}\n}"
 
-clone_format_string="{\n\"%s\":\n{\"ports\":\n{\"1\":\"GHOST\",\n\"2\":\"EMPTY\",\n\"3\":\"EMPTY\",\n\"4\":\"EMPTY\",\n\"5\":\"EMPTY\",\n\"6\":\"EMPTY\"\n},\n\"original\":\"$original_ovx_switch\"}\n}"
+clone_format_string="{\n\"%s\":\n{\"ports\":\n{\"1\":{\"type\":\"GHOST\"},\n\"2\":{\"type\":\"EMPTY\"},\n\"3\":{\"type\":\"EMPTY\"},\n\"4\":{\"type\":\"EMPTY\"},\n\"5\":{\"type\":\"EMPTY\"},\n\"6\":{\"type\":\"EMPTY\"}\n},\n\"original\":\"$original_ovx_switch\"}\n}"
 
 original_data=$(printf "$original_format_string" "$original_ovx_switch")
 
