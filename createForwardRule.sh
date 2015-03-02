fc0a1cdcd3c0df29a49b0adcbf37cb1fc6a92e71
Michael Coughlin <michael.coughlin@colorado.edu>
2015-03-01 18:23:50 -0700

    updated rule creation to take arguments

--- createForwardRule.sh
+++ createForwardRule.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 if [ -z "$1" ];
-then
-	echo "need first port"
+then 
+	echo "Need source port number";
 	exit 1;
 fi
 
 if [ -z "$2" ];
-then
-	echo "need second port"
+then 
+	echo "Need destination port number";
 	exit 1;
 fi
 
-source createForwardRule.sh $1 $2
-source createForwardRule.sh $2 $1
-
-#curl -d '{"switch": "00:a4:23:05:00:00:00:01", "name":"mod-2", "priority":"100", "ingress-port":"3", "active":"true", "actions":"output=2"}' http://192.168.1.3:8080/wm/staticflowentrypusher/json
-#echo ""
+if [ -z "$3" ];
+then 
+	echo "Need dpid";
+	exit 1;
+fi
+curl -d '{"switch": "$3", "name":"mod-$1_to_$2", "priority":"100", "ingress-port":"$1", "active":"true", "actions":"output=$2"}' http://192.168.1.3:8080/wm/staticflowentrypusher/json
+echo ""
+echo "Wrote rule link ports $1 and $2 on switch $3"
 #curl -d '{"switch": "00:a4:23:05:00:00:00:01", "name":"mod-1", "priority":"100", "ingress-port":"2", "active":"true", "actions":"output=3"}' http://192.168.1.3:8080/wm/staticflowentrypusher/json
 #echo ""
 #curl -d '{"switch": "00:a4:23:05:00:00:00:01", "name":"mod-3", "priority":"100", "ingress-port":"1", "vlan-id":"20", "active":"true", "actions":"strip-vlan,output=2"}' http://192.168.1.3:8080/wm/staticflowentrypusher/json
